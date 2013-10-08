2fb3ecdb788ce3c428ff0142b0c929d90e795638
Steve Kondik <shade@chemlab.org>
2013-10-07 23:32:21 -0700

    power: Don't build this if we choose the CM PowerHAL
    Change-Id: I84617f1984d6b49a9e0eca617d2ab5624a27025a

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(TARGET_USES_CM_POWERHAL),true)
 ifneq ($(TARGET_PROVIDES_POWERHAL),true)
 
 LOCAL_PATH := $(call my-dir)
@@ -21,5 +22,6 @@ LOCAL_CFLAGS += -DBOOST_SOCKET=\"$(TARGET_MPDECISION_BOOST_SOCKET)\"
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+endif
 endif
 endif
